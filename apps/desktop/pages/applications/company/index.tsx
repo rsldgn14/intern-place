3dc3491a30de4e9b1df2e507b74df301e1a30615
Mustafa yenişehirli <you@example.com191805024@stu.adu.edu.tr>
2024-03-02 21:11:16 +0300

    my applications filter

--- apps/desktop/pages/applications/company/index.tsx
+++ apps/desktop/pages/applications/company/index.tsx
@@ -1,21 +1,49 @@
 import { css } from '@emotion/react';
-import { CompanyApplicationCard } from '@intern-place/components';
-import { Applications, Companies } from '@intern-place/types';
+import {
+  ApplicationFilter,
+  CompanyApplicationCard,
+} from '@intern-place/components';
+import { Applications, Companies, Notices } from '@intern-place/types';
 import { GetServerSideProps } from 'next';
+import { useEffect, useState } from 'react';
 
 interface Props {
   applications: Applications.Application[];
+  notices: Notices.Notice[];
 }
 
 export default function Index(props: Props) {
-  console.log(props.applications);
+  const [applications, setApplications] = useState<Applications.Application[]>(
+    props.applications
+  );
+  const [filteredData, setFilteredData] = useState<number | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (filteredData) {
+      const filteredApplications = props.applications.filter(
+        (app) => app.NoticeID === filteredData
+      );
+      setApplications(filteredApplications);
+    } else {
+      setApplications(props.applications);
+    }
+  }, [filteredData, props.applications]);
+
   return (
-    <div css={containerCSs}>
-      {props.applications.length > 0
-        ? props.applications.map((app, key) => (
-            <CompanyApplicationCard key={key} application={app} />
-          ))
-        : 'Not found'}
+    <div css={bodyCss}>
+      <ApplicationFilter
+        notices={props.notices}
+        setFilteredData={(id: number) => setFilteredData(id)}
+      />
+      <div css={containerCSs}>
+        {applications.length > 0
+          ? applications.map((app, key) => (
+              <CompanyApplicationCard key={key} application={app} />
+            ))
+          : 'Not found'}
+      </div>
     </div>
   );
 }
@@ -25,6 +53,12 @@ const containerCSs = css`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(2, 1fr);
+`;
+
+const bodyCss = css`
+  display: flex;
+  gap: 50px;
+  padding: 20px;
 `;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -51,9 +85,16 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   });
 
+  const notices = await Notices.mine({
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+
   return {
     props: {
       applications: applications,
+      notices: notices,
     },
   };
 };
--- libs/components/src/lib/ApplicationFilter.tsx
+++ libs/components/src/lib/ApplicationFilter.tsx
@@ -1,2 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
+import ApplicationFilterItem from './ApplicationFilterItem';
+import { Notices } from '@intern-place/types';
 
+interface Props {
+  notices: Notices.Notice[];
+  setFilteredData: (noticeID: number) => void;
+}
 
+export default function ApplicationFilter(props: Props) {
+  const [notices, setNotices] = useState<Notices.Notice[]>(props.notices);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  useEffect(() => {
+    if (searchTerm) {
+      const filteredNotices = props.notices.filter((n) =>
+        n.Title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setNotices(filteredNotices);
+    } else {
+      setNotices(props.notices);
+    }
+  }, [searchTerm, props.notices]);
+
+  return (
+    <div css={appContainerCss}>
+      <div css={leftPanelCss}>
+        <input
+          type="text"
+          placeholder="İlan Ara"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          css={searchInputCss}
+        />
+        {notices.map((n: Notices.Notice, index) => (
+          <ApplicationFilterItem
+            key={index}
+            title={n.Title}
+            onClick={() => props.setFilteredData(n.ID)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const appContainerCss = css`
+  diplay: flex;
+  gap: 300px;
+  min-height: 500px;
+`;
+
+const leftPanelCss = css`
+  padding: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  overflow-y: auto;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const searchInputCss = css`
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
